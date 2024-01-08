f30f19d0a2e99fc977b7e5c4715985c44f159bf7
Swarnava Das <swarnava0802@gmail.com>
2024-01-08 12:46:50 +0530

    change1

--- server.js
+++ server.js
@@ -46,6 +46,7 @@ app.get("/", async (req, res) => {
     res.render("index.ejs", { posts: response });
   } catch (error) {
     res.status(500).json({ message: "Error fetching posts" });
+    console.log(error);
   }
 });
 
